cf3f1855f99fb5ac3948453018c10279080d6227
Mr. Daehn <jdaehn@missouristate.edu>
2019-03-04 16:25:09 -0600

    MASTER - Initial import of source files.

---
+++ src/main/cpp/Main.cpp
@@ -0,0 +1,16 @@
+/**
+ * CSC232 Data Structures
+ * Missouri State University
+ * 
+ * @file   Main.cpp
+ * @author Jim Daehn
+ * @brief  Entry point for hw03 target.
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, main!" << std::endl;
+    return EXIT_SUCCESS;
+}
