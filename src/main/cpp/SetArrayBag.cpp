269b99a7c926ac88b9c3d1ee8a6858ccebd5e736
DaltonMcCart <dmm42@live.missouristate.edu>
2019-03-08 13:16:09 -0600

    Added Longhi Vo as coauthor
    Updated intersectionWith method to account for duplicates in a set with a new method

--- src/main/cpp/SetArrayBag.cpp
+++ src/main/cpp/SetArrayBag.cpp
@@ -5,21 +5,25 @@
  * @file    SetArrayBag.cpp
  * @authors Jim Daehn <jdaehn@missouristate.edu>
  *          Dalton McCart <mccart42@live.missouristate.edu>
+ * @coauthor Longhi Vo <nvo97@live.missouristate.edu>
  *
  * @brief   Implementation file for an set supporting, array-based implementation of the ADT bag.
  */
 
 #include "SetArrayBag.h"
+#include <iostream>
 
 template<typename ItemType>
 BagInterface<ItemType> &SetArrayBag<ItemType>::unionWith(const BagInterface<ItemType> &bag) {
     BagInterface<ItemType> *unionBag = new SetArrayBag{};
 
     for (auto item : ArrayBag<ItemType>::toVector()){
-        unionBag->add(item);
+            unionBag->add(item);
     }
+
+
     for (auto item : bag.toVector()){
-        unionBag->add(item);
+           unionBag->add(item);
     }
     return *unionBag;
 }
@@ -29,7 +33,8 @@ BagInterface<ItemType>& SetArrayBag<ItemType>::intersectionWith(const BagInterfa
     BagInterface<ItemType> *intersectionBag = new SetArrayBag{};
 
     for (auto item : ArrayBag<ItemType>::toVector()){
-        if (bag.contains(item) && !intersectionBag->contains(item)){
+        if (bag.contains(item) && intersectionBag->getFrequencyOf (item) < ArrayBag<ItemType>::getFrequencyOf (item) &&
+            intersectionBag->getFrequencyOf(item) < bag.getFrequencyOf (item) ){
             intersectionBag->add(item);
         }
     }
